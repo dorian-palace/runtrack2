9641f9429da19bfa64049ed3603dcefa922d86ca
dorian-palace <dorian.palace@laplateforme.io>
2021-11-02 10:26:38 +0100

    job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,15 @@
+<?php
+$str = 'Laplateforme';
+$str2 =' Vive';
+$str3 = '!';
+echo $str;
+echo $str2;
+echo $str3;
+echo '<br>'. $str2 . " " .$str .$str3;
+$val = 6;
+echo '<br>'. ($val +4);
+$myBool = 'true';
+echo '<br>'. $myBool;
+$myBool = 'false';
+echo '<br>'. $myBool;
+?>
