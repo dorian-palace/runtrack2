520f956faf0e2e529df970922f3ed780ca8c9774
dorian-palace <dorian.palace@laplateforme.io>
2021-11-02 11:45:53 +0100

    commentaire

--- jour01/job02/index.php
+++ jour01/job02/index.php
@@ -1,6 +1,8 @@
 <?php
 $bonjour = 'Hello';
 $ecole = 'Laplateforme!';
+//Simple echo
 echo 'Hello'. " ". 'Laplateforme!';
+// Hello laplateforme! a l'aide de variables
 echo '<br>'. $bonjour. " ". $ecole;
 ?>
