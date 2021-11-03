4eed9155161f98b393cb95f0535f8612b074c93a
dorian-palace <dorian.palace@laplateforme.io>
2021-11-03 15:30:43 +0100

    jour02 job1.2.3.4

---
+++ jour02/job01/index.php
@@ -0,0 +1,14 @@
+<?php
+$x=0;
+while($x<=1337){
+
+    if ($x==42){
+        echo "<b><u><br> $x</u></br></b>";
+        $x++;
+    }else{
+
+    echo "<br>$x</br>";
+    $x++;
+  }
+}
+?>
---
+++ jour02/job03/index.php
@@ -0,0 +1,18 @@
+<?php
+$pla = "La Plateforme_";
+for($x = 0; $x <= 100; $x++ ){
+    if($x <=20){
+        echo "<i><br>$x</i>";
+    }
+    elseif($x == 42){
+        echo "<br>$pla";
+
+    }elseif($x>=25 && $x<=50){
+        echo "<br><u>$x</u>";
+    }
+    
+    else{
+        echo "<br>$x";
+    }
+
+}
