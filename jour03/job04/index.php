40885b8d5117892f75da080c023f8ecdd02e8f57
dorian-palace <dorian.palace@laplateforme.io>
2021-11-05 11:19:15 +0100

    job4

---
+++ jour03/job04/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$str = 'Dans l\'espace, personne ne
+vous entend crier';
+
+/*for //($i=0; $i <= $str; $i++) {
+
+    if(isset($str[$i]) == $i){
+        echo $i. '<br/>';
+    }
+}*/
+$i = 0;
+
+while(isset($str[$i])){
+    
+    echo $str[$i];
+    $i++; 
+}
+
+//var_dump($i);
+print_r($i);
+?>
