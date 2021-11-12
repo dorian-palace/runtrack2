a9c99ca6979b89aea6d87880162bbf774448ea74
dorian-palace <dorian.palace@laplateforme.io>
2021-11-12 16:49:49 +0100

    job12

---
+++ jour09/job12/index.php
@@ -0,0 +1,60 @@
+<?php
+//mysqli_connet = connexion mysql
+$bdd = mysqli_connect('localhost','root', 'root', 'jour08');
+
+//mysqli_query = executer requete
+$req = mysqli_query($bdd, "SELECT prenom,nom,naissance FROM etudiants WHERE naissance BETWEEN '1998-01-01' AND '2018-01-01'");
+
+//mysqli_fetch_all = pour tout recuperer
+$res= mysqli_fetch_all($req, MYSQLI_ASSOC);
+
+
+echo "<pre>";
+var_dump($res);
+echo "<pre>";
+?>
+
+<table border="1"> 
+    <thead>
+            <?php foreach($res[0] as $key => $value){
+                        echo "<th>$key</th>";
+                    }
+?>    
+        
+    </thead>
+    <tbody>
+        <tr>
+            <?php foreach($res[0] as $key => $value){
+                        echo "<td>$value</td>";
+                    } ?> 
+        </tr>
+
+        <tr>
+            <?php foreach($res[1] as $key => $value){
+                        echo "<td>$value</td>";
+                    } ?> 
+        </tr>
+
+        <tr>
+            <?php foreach($res[2] as $key => $value){
+                        echo "<td>$value</td>";
+                    } ?> 
+        </tr>
+
+        <tr>
+            <?php foreach($res[3] as $key => $value){
+                        echo "<td>$value</td>";
+                    } ?> 
+        </tr>
+
+        <tr>
+            <?php foreach($res[4] as $key => $value){
+                        echo "<td>$value</td>";
+                    } ?> 
+        </tr>
+
+        <tr>
+            <?php foreach($res[5] as $key => $value){
+                        echo "<td>$value</td>";
+                    } ?> 
+        </tr>   
